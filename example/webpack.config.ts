8b303ceef1b2a3a9bf021f949d662215973b65dc
Spencer Miskoviak <smiskoviak@gmail.com>
2019-08-07 11:46:19 -0700

    Start on an example

--- example/webpack.config.ts
+++ example/webpack.config.ts
@@ -1,10 +1,15 @@
-import webpack from "webpack";
+import * as webpack from "webpack";
+import * as path from "path";
 
 import { YarnCheck } from "../src";
 
 const config: webpack.Configuration = {
   mode: "development",
   entry: "./src/index.ts",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
   plugins: [new YarnCheck()]
 };
 
