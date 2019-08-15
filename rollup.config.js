5c5978fb17c8d3855379fe76ce2b33945217e6e8
Spencer Miskoviak <smiskoviak@gmail.com>
2019-08-14 20:20:49 -0700

    fix(tsconfig): set the proper module resolution

--- rollup.config.js
+++ rollup.config.js
@@ -3,13 +3,7 @@ import builtins from 'builtin-modules'
 
 import pkg from "./package.json";
 
-// All dependencies and peer dependencies are considered external dependencies.
-// This means that the consuming application must explicitly have the necessary
-// peer dependencies defined. The dependencies of this package are defined as
-// part of this package and will be installed when this package is installed.
-// This is done to avoid including these as part of this bundle to keep this
-// bundle slim and allow the consuming application to deduplicate packages if
-// possible.
+// All dependencies and node built ins are considered external dependencies.
 const EXTERNALS = [...Object.keys(pkg.dependencies), ...builtins]
 
 const config = {
